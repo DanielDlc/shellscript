cd623a81ce026cba92edbb406fa3089d0b25847c
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-03-25 10:56:29 -0300

    Entrar e sair de um diretório

---
+++ 01_comandos_iniciais/cd_02.sh
@@ -0,0 +1,44 @@
+
+#!/bin/bash
+
+#######################################
+#
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)
+# Data Criação: 25/03/2021
+#
+# Descrição: criação de diretório
+#
+# Exemplo de uso: ./cd.sh
+#
+# Alteracoes
+#	Dia X - Inclusao da Função de ordenacao
+#	Dia Y - Correcao da Função de leitura
+#
+################################################
+
+# entrar e sair de um diretório utilizamos o comando cd
+
+# entrar em um diretório (chamado Desktop) 
+cd Desktop  
+
+# para sair de um diretório (ou retornar para o diretório anterior)
+cd ..
+
+# para entrar nos dois diretórios (podemos apertar o tab para autocompletar) 
+cd Desktop/projeto shell script 
+
+# para retornar dois diretórios
+cd ../..
+
+# para retornar dois diretórios e entrar em outro
+cd ../../Downloads/
+
+# retornar para o diretório anterior 
+cd - 
+
+# retornar para o home 
+cd ~   
+cd
+
+# podemos também entrar no home de outro usuário 
+cd ~nome_do_usuário
