2359ed52802c931b73111a1f50f9342bee515a76
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-03-27 10:56:26 -0300

    criar e atualizar horário do arquivo

---
+++ 01_comandos_iniciais/touch_05.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# criar um arquivo em branco
+touch arquivo-teste
+
+# caso digite o comando touch em um arquivo que já existe, ele atualiza o horário.
+touch arquivo-teste
+
+# podemos facilmente visualizar o arquivo atualizado com o comando
+ls -l
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 27/03/2021				   #
+#							   #
+# Descrição: criar e atualizar arquivos			   #
+#							   #
+# Exemplo de uso: ./touch_05.sh3		           #
+#							   #
+# Alteracoes						   #		   
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
