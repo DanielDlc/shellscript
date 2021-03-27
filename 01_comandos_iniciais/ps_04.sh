12c0e84d1d1744d791aab4432e39acd7f9770ea6
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-03-27 10:36:47 -0300

    mostrar processos com comando ps

---
+++ 01_comandos_iniciais/ps_04.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Mostrar os meus processos em execução na máquina na sessão atual.
+ps
+
+# mostrar o processo que utilizamos
+# nome do usuário, PID é o id do processo, comsumo de CPU, memória e memória virtual,
+# horário de execução quando foi iniciado, o tempo que esta em execução e 
+# nome completo do processo.
+ps axu
+
+# mostrar processo com mais detalhes no cabeçalho
+ps axu | more
+
+# visualizar os processos em forma de árvore
+pstree
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 27/03/2021				   #
+#							   #
+# Descrição: mostrar processos				   #
+#							   #
+# Exemplo de uso: ./ps_03.sh3				   #
+#							   #
+# Alteracoes						   #		   
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
+
