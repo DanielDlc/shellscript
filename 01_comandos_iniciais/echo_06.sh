6d6d55b2c2c3386398211874f9ff84aee7f8cf13
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-03-28 10:42:16 -0300

    exibir mensagens na tela ou em um arquivo

---
+++ 01_comandos_iniciais/echo_06.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Mostrar texto na saída principal.
+echo Linux 
+
+# escrever sem colocar saída 
+echo -n "Linux 2021"  
+
+# habilitar os parâmetros especiais ( \t é um tab)
+echo -e "Linux\t 2021" 
+
+# utilizamos \n para quebra de linha 
+echo -e "Linux\n2021"
+
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 28/03/2021				   #
+#							   #
+# Descrição: produzir as saídas     	 		   #	
+#							   #
+# Exemplo de uso: ./echo_06.sh3				   #
+#							   #
+# Alteracoes						   #		   
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
