53cade42738e1a2f5a12c5cd18f0b1e6fd33dd35
DanielDlc <dlc.engcomputacao@gmail.com>
2021-04-01 11:04:35 -0300

    exibir arquivos de texto

---
+++ 02_comandos_dentro_de_arquivos/01_cat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# comandos executados dentro do diretório (02_comandos_dentro_de_arquivos)
+cd Desktop/shellscript/02_comandos_dentro_de_arquivos/
+
+# exibe o resultado de um arquivo na saída padrão
+cat alunos.txt
+
+# não vai numerar não estejam em branco
+cat -b alunos2.txt
+
+# numera todas as linhas (inclusive as linhas em branco)
+cat -n alunos2.txt
+
+# ostra caracteres especiais (tabulações, espaços entre outros)
+cat -A alunos2.txt
+
+# ler do final para o início
+tac alunos.txt
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 30/03/2021				   #
+#							   #
+# Descrição: exibe na tela                                 #
+#							   #
+# Exemplo de uso: ./01_cat.sh				   #
+#							   #
+# Alteracoes						   #		   
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
+
+
