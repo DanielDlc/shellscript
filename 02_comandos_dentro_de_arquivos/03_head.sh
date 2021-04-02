81f41390f9daf9a24ad32642daee931262b3022d
DanielDlc <dlc.engcomputacao@gmail.com>
2021-04-02 11:17:14 -0300

    mostra início dos dados

---
+++ 02_comandos_dentro_de_arquivos/03_head.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# mostra as primeiras 10 linhas do arquivo por padrão.
+head arquivolongo.txt
+
+# mostra as primeiras 5 linhas do arquivo
+head -n5 arquivolongo.txt
+
+# podemos utilizar apenas -3 sem o (n) para mostrar as primeiras 3 linhas.
+head -3 alunos.txt
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 02/04/2021				   #
+#							   #
+# Descrição: mostra primeiras linhas do arquivo            #
+#							   #
+# Exemplo de uso: ./03_head.sh				   #
+#							   #
+# Alteracoes						   #		   
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
+
+
+
+
