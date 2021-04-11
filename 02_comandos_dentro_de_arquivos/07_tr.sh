85513eb87134f935ccfeac42d65b2514f2d70cd6
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-04-11 11:14:06 -0300

    Substituir e excluir caracteres

---
+++ 02_comandos_dentro_de_arquivos/07_tr.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# mostrar arquivo e trocar a letra ( a minuscula) pela letra (d minuscula)    
+cat alunos.txt | tr a d
+
+# trocar as letras minusculas de (a até z) por letras maiusculas
+cat alunos.txt | tr a-z A-Z
+
+# trocar espaço por tab
+cat alunos2.txt | tr ' ' '\t'
+
+# trocar espaço por tab e verificar.
+cat alunos2.txt | tr ' ' '\t' | cat -A
+
+# deletar caracteres
+cat alunos2.txt | tr -d aei
+
+# trocar uma letra por outra
+echo "danieldlc shellscript" | tr ds DS
+
+# retorna apenas uma unica vez quando existe letra repetida
+echo "ShellScript" | tr -s 'l'
+
+# letras minusculas virarem maiusculas
+echo "danieldlc shell script" | tr [:lower:] [:upper:] 
+
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 11/04/2021				   #
+#							   #
+# Descrição: traduz e deleta caracteres dentro de uma str  #
+#							   #
+# Exemplo de uso: ./07_tr.sh				   #
+#							   #
+# Alteracoes						   #
+#							   #
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
+
+
