71f92322cfc7bc8dcfb5ff976903b2e71cc24020
DanielDlc <dlc.engcomputacao@gmail.com>
2021-04-17 05:23:46 -0300

    gerar sequência de números

---
+++ 02_comandos_dentro_de_arquivos/13_seq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# mostrar sequência de 1 até 10 
+seq 10
+
+# sequência com argumento 
+seq 0 20
+
+# sequência com incremento e argumento
+seq 0 5 50
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 17/04/2021				   #
+#							   #
+# Descrição: gerar sequência de números	         	   #
+#							   #
+# Exemplo de uso: ./13_seq.sh				   #
+#							   #
+# Alteracoes						   #
+#							   #
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
