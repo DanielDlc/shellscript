18f5fdef11da17b40264d254b592d7526a2918e4
DanielDlc <dlc.engcomputacao@gmail.com>
2021-04-17 05:10:47 -0300

    mostrar data e hora

---
+++ 02_comandos_dentro_de_arquivos/12_date.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# mostrar somente data e hora 
+date
+
+# mostrar apenas o digito da hora
+date +%H
+
+# mostrar apenas os digitos do minuto
+date +%M
+
+# mostrar o mês (m minusculo)
+date +%m
+
+# mostrar apenas a data (mês, dia e ano)
+date +%D
+
+# mostrar o dia apenas (d minusculo)
+date +%d
+
+# mostrar dia e mês
+date +%m/d
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 17/04/2021				   #
+#							   #
+# Descrição: mostra data e hora do sistema 		   #
+#							   #
+# Exemplo de uso: ./12_date.sh				   #
+#							   #
+# Alteracoes						   #
+#							   #
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
+
+
+
+
