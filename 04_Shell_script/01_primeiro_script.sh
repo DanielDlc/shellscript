06018d04de22f0b3b8785e462c8c8618b16317a3
DanielDlc <dlc.engcomputacao@gmail.com>
2021-04-29 11:04:50 -0300

    primeiro script em shell

---
+++ 04_Shell_script/01_primeiro_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# exibir da data e hora e lista de alunos
+clear
+echo "================ Meu Primeiro Script ================"
+echo""
+echo -n "Exibir data e hora atual: "
+date 
+echo""
+echo "====================================================="
+echo""
+echo "Listagem de alunos "
+sort ../04_Shell_script/alunos2.txt
+
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 29/04/2021				   #
+#							   #
+# Descrição: primeiro script 				   #
+#							   #
+# Exemplo de uso: ./primeiro_script.sh			   #
+#							   #
+# Alteracoes						   #
+#							   #
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
+
+
+
