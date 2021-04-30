6b1faa7871f0a2b45842fbcec9b318624dc58e58
DanielDlc <dlc.engcomputacao@gmail.com>
2021-04-30 11:19:04 -0300

    criando variaveis

---
+++ 04_Shell_script/02_variaveis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# data e hora com variavel
+DATAHORA=$(date =%H:5M)
+
+# Mostrar lista de alunos 
+ALUNOS=/home/daniel/Desktop/shellscript/04_Shell_script/alunos.txt
+
+
+echo "Exibir data e hora atual: $DATAHORA"
+echo""
+
+
+############################################################
+#							   #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
+# Data Criação: 30/04/2021				   #
+#							   #
+# Descrição: variavel shell  				   #
+#							   #
+# Exemplo de uso: ./variaveis.sh			   #
+#							   #
+# Alteracoes						   #
+#							   #
+#	Dia X - Inclusao da Função de ordenacao		   #
+#	Dia Y - Correcao da Função de leitura		   #
+#							   #
+############################################################
+
+
+
