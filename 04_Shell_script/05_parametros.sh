e60a6101ad245829ac771b0a35bd89160b8936bb
DanielDlc <dlc.engcomputacao@gmail.com>
2021-05-04 10:59:57 -0300

    Entradas de usuário com parâmetro

---
+++ 04_Shell_script/05_parametros.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Parâmetros
+# $0   - nome do programa
+# $#   - quantidade de Parâmetros
+# $*   - todos os parâmetros inseridos
+# $1-9 - cada um dos parâmetros
+
+echo "O script $0 recebeu $# parâmetros"
+
+echo "Os parâmetros recebidos foram: $*"
+
+echo""
+
+echo "Parâmetro \$1 = $1"
+echo "Parâmetro \$2 = $2"
+echo "Parâmetro \$3 = $3"
+echo "Parâmetro \$4 = $4"
+
+
+####################################################################
+#                                                                  #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)          #
+# Data criação: 04/05/2021                                         #
+#                                                                  #
+# Descrição: Parâmetros                                            #
+#                                                                  #
+# Exemplo de uso: ./05_parametros.sh curso shell script Daniel dlc #
+#                                                                  #
+# Alterações                                                       #
+#                                                                  #
+# Dia X - ...                                                      #
+# Dia Y - ...                                                      #
+#                                                                  #
+####################################################################
