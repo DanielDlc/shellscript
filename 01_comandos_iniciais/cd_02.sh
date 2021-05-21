bb16656719946985a2d63ee03b0b9f9aecf19ab4
DanielDlc <dlc.engcomputacao@gmail.com>
2021-05-21 11:03:00 -0300

    Entrar e sair de um diretório

--- 01_comandos_iniciais/cd_02.sh
+++ 01_comandos_iniciais/cd_02.sh
@@ -1,31 +1,16 @@
 
 #!/bin/bash
 
-#######################################
-#
-# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)
-# Data Criação: 25/03/2021
-#
-# Descrição: entrar, sair, modificar um diretório
-#
-# Exemplo de uso: ./cd.sh
-#
-# Alteracoes
-#	Dia X - Inclusao da Função de ordenacao
-#	Dia Y - Correcao da Função de leitura
-#
-################################################
-
 # entrar e sair de um diretório utilizamos o comando cd
 
-# entrar em um diretório (chamado Desktop) 
-cd Desktop  
+# entrar em um diretório (chamado Desktop)
+cd Desktop
 
 # para sair de um diretório (ou retornar para o diretório anterior)
 cd ..
 
 # para entrar nos dois diretórios (podemos apertar o tab para autocompletar) 
-cd Desktop/projeto shell script 
+cd Desktop/projeto shell script
 
 # para retornar dois diretórios
 cd ../..
@@ -33,12 +18,28 @@ cd ../..
 # para retornar dois diretórios e entrar em outro
 cd ../../Downloads/
 
-# retornar para o diretório anterior 
-cd - 
+# retornar para o diretório anterior
+cd -
 
-# retornar para o home 
-cd ~   
+# retornar para o home
+cd ~
 cd
 
-# podemos também entrar no home de outro usuário 
+# podemos também entrar no home de outro usuário
 cd ~nome_do_usuário
+
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data de criação: 25/03/2021                             #
+#                                                         #
+# Descrição: entrar e sair de um diretório                #
+#                                                         #
+# Exemplo de uso: ./cd.sh                                 #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia 21/05/16                                            #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
