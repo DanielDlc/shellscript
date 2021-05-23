0d47ba829997e8e7d4017dfb5d9e91c810520d60
DanielDlc <dlc.engcomputacao@gmail.com>
2021-05-23 10:58:20 -0300

    listar diretórios e mostrar diretório atual

--- 01_comandos_iniciais/ls_03.sh
+++ 01_comandos_iniciais/ls_03.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-############################################################
-#							   #
-# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
-# Data Criação: 26/03/2021				   #
-#							   #
-# Descrição: mostrar diretório atual e listar diretórios   #
-#							   #
-# Exemplo de uso: ./ls_03.sh3				   #
-#							   #
-# Alteracoes						   #		   
-#	Dia X - Inclusao da Função de ordenacao		   #
-#	Dia Y - Correcao da Função de leitura		   #
-#							   #
-############################################################
-
 # Mostrar o caminho completo do diretório (irá aparecer /home/Nome_do_usuário)
 pwd  # ( /home/daniel)
 
@@ -25,7 +10,7 @@ ls
 ls -ltr
 
 # mostrar detalhes de arquivos e diretório podemos utilizar
-ls -l 
+ls -l
 
 # mostrar arquivos / diretório sem os detalhes
 ls -1
@@ -35,3 +20,19 @@ ls *.sh
 
 # mostrar arquivos ocultos
 ls -a
+
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data de criação: 26/03/2021                             #
+#                                                         #
+# Descrição: mostrar diretório atual e listar diretórios  #
+#                                                         #
+# Exemplo de uso: ./ls_03.sh3                             #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
