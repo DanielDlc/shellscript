d09f56492b24641e8754c2b5f8068de2fa445f78
DanielDlc <dlc.engcomputacao@gmail.com>
2021-05-26 10:41:10 -0300

    mostrar texto na saída padrão

--- 01_comandos_iniciais/echo_06.sh
+++ 01_comandos_iniciais/echo_06.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # Mostrar texto na saída principal.
-echo Linux 
+echo Linux
 
-# escrever sem colocar saída 
-echo -n "Linux 2021"  
+# escrever sem colocar saída
+echo -n "Linux 2021"
 
 # habilitar os parâmetros especiais ( \t é um tab)
-echo -e "Linux\t 2021" 
+echo -e "Linux\t 2021"
 
-# utilizamos \n para quebra de linha 
+# utilizamos \n para quebra de linha
 echo -e "Linux\n2021"
 
-
-############################################################
-#							   #
-# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
-# Data Criação: 28/03/2021				   #
-#							   #
-# Descrição: produzir as saídas     	 		   #	
-#							   #
-# Exemplo de uso: ./echo_06.sh				   #
-#							   #
-# Alteracoes						   #		   
-#	Dia X - Inclusao da Função de ordenacao		   #
-#	Dia Y - Correcao da Função de leitura		   #
-#							   #
-############################################################
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data de criação: 28/03/2021                             #
+#                                                         #
+# Descrição: imprimir informação na tela                  #
+#                                                         #
+# Exemplo de uso: ./echo_06.sh                            #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
