3cbf0a5530217f34d09c06a9bd31a22f8cef66cf
DanielDlc <dlc.engcomputacao@gmail.com>
2021-05-27 08:28:23 -0300

    ler e-mail enviado no shell

---
+++ 09_e-mail/02_mail.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# abrir caixa de mensagem do usuário atual (digitar 1 para ler a mensagem)
+mail
+
+# iremos observar que o conteúdo do e-mail é o mesmo que estava no arquivo.
+
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data criação: 27/05/2021                                #
+#                                                         #
+# Descrição: ler mensagem enviada                         #
+#                                                         #
+# Exemplo de uso: ./02_mail.sh                            #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
