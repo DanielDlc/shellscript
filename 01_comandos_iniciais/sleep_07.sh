31de2300af0a87ce3ebf5dd38cedcad1799443f0
DanielDlc <dlc.engcomputacao@gmail.com>
2021-05-28 09:03:09 -0300

    temporização da execução e limpar conteúdo da tela

--- 01_comandos_iniciais/sleep_07.sh
+++ 01_comandos_iniciais/sleep_07.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Podemos forçar a espera de um certo período de tempo entre a execução de comandos 
+# Podemos forçar a espera de um certo período de tempo entre a execução de comandos
 sleep 7
 
 # limpar tela
 clear  # podemos também utilizar o atalho (ctrl + l) no teclado.
 
 
-############################################################
-#							   #
-# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
-# Data Criação: 30/03/2021				   #
-#							   #
-# Descrição: temporização da execução                      #
-#							   #
-# Exemplo de uso: ./sleep_07.sh				   #
-#							   #
-# Alteracoes						   #		   
-#	Dia X - Inclusao da Função de ordenacao		   #
-#	Dia Y - Correcao da Função de leitura		   #
-#							   #
-############################################################
-
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data de criação: 28/03/2021                             #
+#                                                         #
+# Descrição: temporização de execução                     #
+#                                                         #
+# Exemplo de uso: ./sleep_07.sh                           #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
