73cba6c29696b735fb33ee2c610c3e90cfba9214
DanielDlc <dlc.engcomputacao@gmail.com>
2021-05-30 10:56:55 -0300

    cópia de diretórios e arquivos

--- 01_comandos_iniciais/08_cp.sh
+++ 01_comandos_iniciais/08_cp.sh
@@ -8,23 +8,24 @@ cp -i mkdir_01.sh mkdir.bak
 
 # se repetirmos o mesmo processo acima (sobrescrever y ou n)
 cp -i mkdir_01.sh mkdir.bak
-y  
+y
 
 # copiar recursivamente pasta de Documentos para Documentos2 (toda arvore abaixo dele)
 cp -r Documentos/ Documentos2
 
-############################################################
-#							   #
-# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
-# Data Criação: 22/4/2021				   #
-#							   #
-# Descrição: Cópia de arquivos e diretórios         	   # 
-#							   #
-# Exemplo de uso: ./08_cp.sh				   #
-#							   #
-# Alteracoes						   #
-#							   #
-#	Dia X - Inclusao da Função de ordenacao		   #
-#	Dia Y - Correcao da Função de leitura		   #
-#							   #
-############################################################
+
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data de criação: 22/04/2021                             #
+#                                                         #
+# Descrição: cópia de arquivos e diretórios               #
+#                                                         #
+# Exemplo de uso: ./08_cp.sh                              #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
