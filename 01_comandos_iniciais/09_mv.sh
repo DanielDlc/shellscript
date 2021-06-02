8e8a15f3663c8c82fb5a367d7f1186746b72327e
DanielDlc <dlc.engcomputacao@gmail.com>
2021-06-02 08:58:27 -0300

    mover ou renomear um diretório

--- 01_comandos_iniciais/09_mv.sh
+++ 01_comandos_iniciais/09_mv.sh
@@ -8,20 +8,21 @@ mv mkdir_01.sh 01_mkdir.sh
 
 # podemos utilizar o comando | progress -m (para ver o progresso)
 # se o arquivo for muito longo ou se utilizarmos cp para copiar.
-mv mkdir_01.sh /home/daniel/Documentos | progress -m 
+mv mkdir_01.sh /home/daniel/Documentos | progress -m
 
-############################################################
-#							   #
-# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
-# Data Criação: 22/4/2021				   #
-#							   #
-# Descrição: mover diretório e renomear 		   #               
-#							   #
-# Exemplo de uso: ./09_cp.sh				   #
-#							   #
-# Alteracoes						   #
-#							   #
-#	Dia X - Inclusao da Função de ordenacao		   #
-#	Dia Y - Correcao da Função de leitura		   #
-#							   #
-############################################################
+
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data de criação: 22/04/2021                             #
+#                                                         #
+# Descrição: mover diretório e renomear                   #
+#                                                         #
+# Exemplo de uso: ./09_mv.sh                              #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
