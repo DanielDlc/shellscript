3e3aa03ea6674297246fc3d09f4584babf812034
DanielDlc <dlc.engcomputacao@gmail.com>
2021-06-04 08:47:24 -0300

    compactar arquivo

---
+++ 02_comandos_dentro_de_arquivos/22_tar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+# compactando arquivolongo.txt
+# c - cria um novo arquivo .tar, v - mostra uma descrição do processo e f - nome do arquivo
+tar -cvf arquivolongo.tar /home/daniel/Desktop/shellscript/02_comandos_dentro_de_arquivos/arquivolongo.txt
+
+
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data criação: 4/06/2021                                 #
+#                                                         #
+# Descrição: compactando arquivo                          #
+#                                                         #
+# Exemplo de uso: ./tar.sh                                #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
