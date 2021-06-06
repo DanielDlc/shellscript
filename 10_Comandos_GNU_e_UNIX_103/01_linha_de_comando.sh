5b7ed8f63595601af78ba42a3ef6b54475f2b36c
DanielDlc <dlc.engcomputacao@gmail.com>
2021-06-06 08:47:49 -0300

    linha de comando

---
+++ 10_Comandos_GNU_e_UNIX_103/01_linha_de_comando.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# mostrar bash atual
+echo $SHELL
+
+# mostrar tipo de comando, se é interno ou externo.
+type echo  # mostra como um comando interno como builtin
+type tar  # mostra um comando externo como /bin/tar
+
+# localizar os comandos externos em execução no linux
+echo $PATH
+
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data criação: 06/06/2021                                #
+#                                                         #
+# Descrição: trabalhando com linha de comando             #
+#                                                         #
+# Exemplo de uso: ./01_linha_de_comando.sh                #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
