5f43de974582ed8892a9a3a1ad3a55fe29d8aaf3
DanielDlc <dlc.engcomputacao@gmail.com>
2021-06-09 00:03:02 -0300

    mostra as primeiras linhas de um arquivo

--- 02_comandos_dentro_de_arquivos/03_head.sh
+++ 02_comandos_dentro_de_arquivos/03_head.sh
@@ -9,21 +9,19 @@ head -n5 arquivolongo.txt
 # podemos utilizar apenas -3 sem o (n) para mostrar as primeiras 3 linhas.
 head -3 alunos.txt
 
-############################################################
-#							   #
-# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com)  #
-# Data Criação: 02/04/2021				   #
-#							   #
-# Descrição: mostra primeiras linhas do arquivo            #
-#							   #
-# Exemplo de uso: ./03_head.sh				   #
-#							   #
-# Alteracoes						   #		   
-#	Dia X - Inclusao da Função de ordenacao		   #
-#	Dia Y - Correcao da Função de leitura		   #
-#							   #
-############################################################
 
-
-
-
+###########################################################
+#                                                         #
+# Autor: Daniel Louro Costa (dlc.engcomputacao@gmail.com) #
+# Data de criação: 02/04/2021                             #
+#                                                         #
+# Descrição: mostrar as primeiras linhas do arquivo       #
+#                                                         #
+# Exemplo de uso: ./02_head.sh                            #
+#                                                         #
+# Alterações                                              #
+#                                                         #
+# Dia X - ...                                             #
+# Dia Y - ...                                             #
+#                                                         #
+###########################################################
